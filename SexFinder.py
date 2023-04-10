6ac9d9b8b592ad00d5f8a7c61a7cea110a8c3f15
Gaorui Gong <two216@qq.com>
2023-04-10 15:51:44 +0800

    Update SexFinder.py

--- SexFinder.py
+++ SexFinder.py
@@ -1,6 +1,8 @@
 import sys
 import gzip
-def filter_vcf(vcf):
+def filter_vcf(femalelist , malelist, vcf):
+    female =  [line.strip() for line in open(sys.argv[1])]
+    male =  [line.strip() for line in open(sys.argv[2])]
     with gzip.open(vcf , "rt",encoding = "utf-8" ) as txt:
         for line in txt:
             if not line.startswith("#"):
@@ -8,8 +10,6 @@ def filter_vcf(vcf):
                 Chr = line[0]
                 pos = line[1]
                 p = line[6]
-                female = [12, 16, 18, 19, 23, 25, 26, 30, 31, 32, 34, 35, 37, 38, 39, 40, 42, 46, 47, 48]
-                male = [9, 10, 11, 13, 14, 15, 17, 20, 21, 22, 24, 27, 28, 29, 33, 36, 41, 43, 44, 45]
                 maleP = 0
                 femaleP = 0
                 if p == "PASS":
@@ -28,4 +28,4 @@ def filter_vcf(vcf):
 
                     if (maleP >=18 ) and (femaleP >= 18 ):
                         print("\t".join(line))
-filter_vcf(sys.argv[1] )
+filter_vcf(sys.argv[1]  , sys.argv[2] , sys.argv[3])
